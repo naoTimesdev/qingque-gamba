50cfecff899b4a17eff49c9db1f879101a36907b
noaione <noaione0809@gmail.com>
2023-09-07 17:35:35 +0800

    utils: create unity rich to markdown converter

--- qingque/utils.py
+++ qingque/utils.py
@@ -25,11 +25,13 @@ SOFTWARE.
 from __future__ import annotations
 
 import re
+from typing import Callable
 
 __all__ = (
     "get_indexed",
     "complex_walk",
     "strip_unity_rich_text",
+    "unity_rich_to_markdown",
 )
 
 _UNITY_RT_SIZE = re.compile(r"<size=(?:\d+)>(.+?)</size>")
@@ -79,6 +81,10 @@ def complex_walk(dictionary: dict | list, paths: str):
     return dictionary
 
 
+def urich_re(format: str):
+    return re.compile(rf"<{format}>(.+?)</{format}>", re.I)
+
+
 def strip_unity_rich_text(text: str):
     basic_format = ["b", "i", "unbreak", "s", "u", "lowercase", "uppercase", "smallcaps", "nobr", "sup"]
     for tag in basic_format:
@@ -88,3 +94,61 @@ def strip_unity_rich_text(text: str):
     for tag in complex_formats:
         text = tag.sub(r"\1", text)
     return text
+
+
+def _unity_rich_lowercase(m: re.Match[str]) -> str:
+    return m.group(1).lower()
+
+
+def _unity_rich_uppercase(m: re.Match[str]) -> str:
+    return m.group(1).upper()
+
+
+def to_superscript(text: str) -> str:
+    normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-=()"
+    subtitute = "ᴬᴮᶜᴰᴱᶠᴳᴴᴵᴶᴷᴸᴹᴺᴼᴾQᴿˢᵀᵁⱽᵂˣʸᶻᵃᵇᶜᵈᵉᶠᵍʰᶦʲᵏˡᵐⁿᵒᵖ۹ʳˢᵗᵘᵛʷˣʸᶻ⁰¹²³⁴⁵⁶⁷⁸⁹⁺⁻⁼⁽⁾"
+    transltable = text.maketrans("".join(normal), "".join(subtitute))
+    return text.translate(transltable)
+
+
+def to_subscript(text: str) -> str:
+    normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-=()"
+    subtitute = "ₐ₈CDₑբGₕᵢⱼₖₗₘₙₒₚQᵣₛₜᵤᵥwₓᵧZₐ♭꜀ᑯₑբ₉ₕᵢⱼₖₗₘₙₒₚ૧ᵣₛₜᵤᵥwₓᵧ₂₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎"  # noqa: RUF001
+    transltable = text.maketrans("".join(normal), "".join(subtitute))
+    return text.translate(transltable)
+
+
+def to_smallcaps(text: str) -> str:
+    normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-=()"
+    subtitute = "ABCDEFGHIJKLMNOPQRSTUVWXYZᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ0123456789+-=()"  # noqa: RUF001
+    transltable = text.maketrans("".join(normal), "".join(subtitute))
+    return text.translate(transltable)
+
+
+def unity_rich_to_markdown(text_data: str) -> str:
+    # Sorted by priority
+    main_format: dict[re.Pattern[str], str | Callable[[re.Match[str]], str]] = {
+        urich_re("b"): "**",
+        urich_re("i"): "*",
+        urich_re("u"): "__",
+        urich_re("s"): "~~",
+        urich_re("sup"): lambda m: to_superscript(m.group(1)),
+        urich_re("smallcaps"): lambda m: to_smallcaps(m.group(1)),
+        urich_re("sub"): lambda m: to_subscript(m.group(1)),
+        urich_re("lowercase"): _unity_rich_lowercase,
+        urich_re("uppercase"): _unity_rich_uppercase,
+    }
+    strip_format = ["unbreak", "nobr"]
+    for tag in strip_format:
+        text_data = text_data.replace(f"<{tag}>", "").replace(f"</{tag}>", "")
+
+    complex_formats = [_UNITY_RT_SIZE, _UNITY_RT_COLOR, _UNITY_RT_MAT]
+    for tag in complex_formats:
+        text_data = tag.sub(r"\1", text_data)
+
+    for formatter, replacer in main_format.items():
+        if callable(replacer):
+            text_data = formatter.sub(replacer, text_data)
+        else:
+            text_data = formatter.sub(rf"{replacer}\1{replacer}", text_data)
+    return text_data
